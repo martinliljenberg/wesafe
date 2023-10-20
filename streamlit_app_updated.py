e24daad2303f95befc4235f2a70858601c91bfa7
martinliljenberg <39955271+martinliljenberg@users.noreply.github.com>
2023-10-20 14:36:29 +0200

    Add files via upload

---
+++ streamlit_app_updated.py
@@ -0,0 +1,35 @@
+
+import streamlit as st
+import requests
+import pandas as pd
+
+def check_health(tenant_name):
+    # Normally, you'd call Microsoft Graph API or other APIs here
+    # For demonstration, we'll use a mock response.
+    api_response = [
+        {'Service': 'Azure', 'Status': 'No Issues'},
+        {'Service': 'Microsoft 365', 'Status': 'Minor Issues'},
+        {'Service': 'Dynamics 365', 'Status': 'Major Issues'}
+    ]
+    return pd.DataFrame(api_response)
+
+# Streamlit app
+st.title('Check Microsoft 365 and Azure Health')
+st.image('your_logo_here.png', use_column_width=True)
+
+# Layout similar to LinkedIn
+col1, col2 = st.columns((1,3))
+
+with col1:
+    st.write("## Account Info")
+    st.write("Name: John Doe")
+    st.write("Tenant: Contoso")
+    st.image('profile_picture_here.png', use_column_width=True)
+
+with col2:
+    tenant_name = st.text_input('Enter Tenant Name:', '')
+    
+    if st.button('Check'):
+        st.write('## Results:')
+        result = check_health(tenant_name)
+        st.table(result)
